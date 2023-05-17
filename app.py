aaee5c97b2d48054a94a6b418ad83529b8d53ba4
csallahbakash <khajasabengr@gmail.com>
2023-05-17 12:18:06 +0530

    changing username

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+import streamlit as st
+
+# Streamlit application code
+def main():
+    """
+    my doc string
+    """
+    st.title("My Streamlit App")
+    st.write("Welcome to my Streamlit app!")
+
+    # Add your Streamlit app code here
+
+if __name__ == "__main__":
+    main()
